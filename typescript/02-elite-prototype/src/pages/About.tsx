1eda5d10f7b64e5ab0486d7e5c3275c5ce900164
Serge <sajghaa@gmail.com>
2026-06-29 23:04:37 +0200

    Added new files in pages

---
+++ typescript/02-elite-prototype/src/pages/About.tsx
@@ -0,0 +1,4 @@
+const About = () => {
+  return <div className="p-8 text-2xl">🏠 About</div>
+}
+export default About
---
+++ typescript/02-elite-prototype/src/pages/Home.tsx
@@ -0,0 +1,4 @@
+const Home = () => {
+  return <div className="p-8 text-2xl">🏠 Home</div>
+}
+export default Home
